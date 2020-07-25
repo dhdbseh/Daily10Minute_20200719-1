8e549ff8d8cc84da79d03dfe98a7fbb0f69783c9
yd27 <dhdbseh5oe@naver.com>
2020-07-25 14:43:40 +0900

    사용자 명단 받아오기 요청 - ViewOngoing 에러

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/ViewOngoingUsersActivity.kt
@@ -2,8 +2,20 @@ package kr.co.tjoeun.daily10minute_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.tjoeun.daily10minute_20200719.utils.ServerUtil
+
+
+var mProjectId
 
 class ViewOngoingUsersActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_ongoing_users)
+        setupEvents()
+        setValues()
+    }
+
     override fun setupEvents() {
 
     }
@@ -12,12 +24,17 @@ class ViewOngoingUsersActivity : BaseActivity() {
 
         mProjectId = intent.getIntExtra("projectId", 0)
 
+        getOngoingUsersFromServer()
+
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_ongoing_users)
-        setupEvents()
-        setValues()
+    //    진행중인 사람 명단 + 상세정보 물러오기
+    fun getOngoingUsersFromServer() {
+
+        ServerUtil.getRequestProjectDetailWithUser(
+            mContext,
+            mProjectId,
+            object : ServerUtil.JsonResponseHandler)
     }
+
 }
