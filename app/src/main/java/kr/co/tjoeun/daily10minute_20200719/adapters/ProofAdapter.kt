36835d02df32692cbdf9bd7953dc231d9d5a37a0
yd27 <dhdbseh5oe@naver.com>
2020-07-26 14:13:33 +0900

    인증 일시 표시

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/adapters/ProofAdapter.kt
@@ -11,6 +11,7 @@ import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10minute_20200719.R
 import kr.co.tjoeun.daily10minute_20200719.datas.Project
 import kr.co.tjoeun.daily10minute_20200719.datas.Proof
+import java.text.SimpleDateFormat
 
 class ProofAdapter(
     val mContext:Context,
@@ -28,9 +29,9 @@ class ProofAdapter(
         val row = tempRow!!
 
         val userProfileImg = row.findViewById<ImageView>(R.id.userProfileImg)
-        val userNickNameTxt = row.findViewById<ImageView>(R.id.userNickNameTxt)
-        val proofTimeTxt = row.findViewById<ImageView>(R.id.proofTimeTxt)
-        val contentTxt = row.findViewById<ImageView>(R.id.contentTxt)
+        val userNickNameTxt = row.findViewById<TextView>(R.id.userNickNameTxt)
+        val proofTimeTxt = row.findViewById<TextView>(R.id.proofTimeTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
         val proofImg = row.findViewById<ImageView>(R.id.proofImg)
 
         val data = mList[position]
@@ -39,6 +40,13 @@ class ProofAdapter(
 
         userNickNameTxt.text = data.user.nickName
         Glide.with(mContext).load(data.user.profileImageList[0].imageUrl).into(userProfileImg)
+
+//        인증 일시 : 2020년 6월 9일 오전 2시 8분 양식으로 출력
+//        data의 proofTime 변수를 활용하자
+//        String으로 변환 -> SimpleDateFormat의 format 기능 활용
+
+        val sdf = SimpleDateFormat("yyyy년 M월 d일 a h시 m분")
+        proofTimeTxt.text = sdf.format(data.proofTime.time)
 
         return row
     }
