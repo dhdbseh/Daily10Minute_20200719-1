cece875fbd29fb98fb7c364dfa59ba2befef6d9f
yd27 <dhdbseh5oe@naver.com>
2020-07-26 12:18:41 +0900

    참여 인증 시간 파싱

--- app/src/main/java/kr/co/tjoeun/daily10minute_20200719/datas/Proof.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minute_20200719/datas/Proof.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.daily10minute_20200719.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -24,6 +25,19 @@ class Proof {
 //            재료로 들어오는 json을 가지고 -> p의 멤버 변수들의 내용을 채워주자.
             p.id = json.getInt("id")
             p.content = json.getString("content")
+
+//            서버에서는 인증일시를 String으로 알려줌.
+//            앱에서는 인증일시를 Calendar로 저장해야함.
+//            String -> Calendar 변환. SimpleDataString
+
+            val proofTimeString = json.getString("proof_time")
+
+//            서버에서 내려주는 양식을 읽어올 SimpleDateFormat 생성
+//            2020-06-09 02:53:34 양식 분석
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+//            sdf를 이용하서 -> 캘린더 변수에 시간 대입
+            p.proofTime.time = sdf.parse(proofTimeString)
 
             return p
         }
